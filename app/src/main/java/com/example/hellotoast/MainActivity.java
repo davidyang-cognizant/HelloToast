fb33bf629a1b4f1e6202bebe25d4a7bd59bc6d2f
David Yang <davidyang.cognizant@gmail.com>
2022-07-18 13:44:21 -0500

    Contains Shared Preferences

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -10,12 +11,47 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private int mCount = 0;
     private TextView mShowCount;
+    private SharedPreferences mPreferences;
+    private String sharedPrefFile =
+            "com.example.android.hellotoast";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        mPreferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+//        if (savedInstanceState != null) {
+//            mCount = savedInstanceState.getInt("count", 0);
+//            if (mCount != 0) {
+//                mShowCount.setText(String.format("%s", mCount));
+//                Toast.makeText(this, mCount, Toast.LENGTH_SHORT).show();
+//            }
+//        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt("count", mCount);
+        preferencesEditor.apply();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        restoreData();
+    }
+
+    private void restoreData() {
+        //read the data from the file
+        SharedPreferences preferences = getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        mCount = preferences.getInt("count", 0);
+        //put it back into the edittext
+        mShowCount.setText(String.valueOf(mCount));
+        Toast.makeText(this, String.valueOf(mCount), Toast.LENGTH_SHORT).show();
+
     }
 
     public void showToast(View view) {
@@ -24,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         mCount++;
-        if(mShowCount != null){
+        if (mShowCount != null) {
             mShowCount.setText(String.valueOf(mCount));
         }
     }
